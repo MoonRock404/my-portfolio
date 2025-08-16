f3fad3e8d30b297c5a7bc13932b7da821cbe5a50
Chandini <chandinick7@gmail.com>
2025-08-16 01:27:05 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
